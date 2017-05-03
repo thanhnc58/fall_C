924f44b77c3c5ec07c4c621fdaed164a462ee011
thanhnc58 <thanhnc_58@uet.vnu.edu.vn>
2017-05-03 09:46:09 +0700

    fix mhi

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@
 using namespace cv;
 using namespace std;
 
-const double MHI_DURATION = 0.7;
+const double MHI_DURATION = 30;
 const double MAX_TIME_DELTA = 0.5;
 const double MIN_TIME_DELTA = 0.05;
 const double MHI_THRESHOLD = 100;
@@ -39,6 +39,7 @@ Mat mhi; // MHI
 int prev_area , large_motion_frame , move_down_frame , apparently_fall_frame;
 bool DRAW = false;
 mutex m;
+int mhi_timestamp = 0;
 
 void read_next_frame(VideoCapture cap, Mat& frame, int& index){
 
@@ -90,12 +91,6 @@ void Draw_max_contour(Mat foreground, vector<vector<Point> > contours,
                 min_rect = minAreaRect(contours[i]);    // Find the bounding rectangle for biggest contour
             }
         }
-        try{
-            min_elip = fitEllipse(Mat(contours[largest_contour_index]));
-        }
-        catch (exception& e){
-            cout << "Standard exception: " << e.what() << endl;
-        }
 
 
 
@@ -112,8 +107,9 @@ void Draw_max_contour(Mat foreground, vector<vector<Point> > contours,
 }
 
 void  update_mhi( const Mat& img, Mat& mhi_out, int diff_threshold){
-
-    double timestamp = (double)clock() /CLOCKS_PER_SEC; // get current time in seconds
+    mhi_timestamp = mhi_timestamp + 2;
+    double timestamp = mhi_timestamp; // get current time in seconds
+    //cout <<"timestaep: "<< timestamp << endl;
     int foreground_h, foreground_w, mhi_h, mhi_w, foreground_area;
     bool noise;
     vector<vector<Point> > contours , cur_contour;
@@ -167,7 +163,7 @@ void mhi_coefficient(const Mat& img, Mat& mhi_out, int diff_threshold,
 
     threshold( mhi_out, recent_motion, MHI_THRESHOLD, 255, CV_THRESH_BINARY );
     findContours( recent_motion, contours, hierarchy, 1, 2);
-    cout<<"contours size = "<< contours.size() << "//////////////";
+    //cout<<"contours size = "<< contours.size() << "//////////////";
     if (contours.size()>0){
 
         // calculate center of mass of mhi
@@ -256,7 +252,35 @@ void mhi_coefficient(const Mat& img, Mat& mhi_out, int diff_threshold,
 
 }
 
-void shape_feature(Mat frame, RotatedRect min_elip, float& elip_angle, float& elip_ratio, RotatedRect min_rect){
+void shape_feature(Mat frame, Mat motion, float& elip_angle, float& elip_ratio){
+    RotatedRect min_rect;
+    int largest_contour_index = 0;
+    Mat elip_motion;
+    RotatedRect min_elip;
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    threshold( motion, elip_motion, 240, 255, CV_THRESH_BINARY );
+    //imshow("elip_motionnnn",elip_motion);
+    findContours( elip_motion, contours, hierarchy, 1, 2);
+    if (contours.size()>0){
+        double largest_area = contourArea( contours[0]);
+        min_rect = minAreaRect(contours[0]);
+        for(int i = 0 ; i < contours.size() ; i++){
+            double area = contourArea( contours[i]);
+            if(area > largest_area){
+                largest_area = area;
+                largest_contour_index = i;                  //Store the index of largest contour
+                min_rect = minAreaRect(contours[i]);    // Find the bounding rectangle for biggest contour
+            }
+        }
+        //cout <<" test contour" <<contours.size() << endl;
+        try{
+            min_elip = fitEllipse(Mat(contours[largest_contour_index]));
+        }
+        catch (exception& e){
+            cout << "Standard exception: " << e.what() << endl;
+        }
+    }
     try{
         elip_angle = min_rect.angle;
         elip_ratio = min_elip.size.width / min_elip.size.height;
@@ -379,7 +403,7 @@ void run_get_feature(VideoCapture cap, Mat& frame, int& frame_index,
     mhi_coefficient(max_contour, motion, 30 , magnitude, mass_speed, angle, motion_ratio, min_rect);
 
     // calculate coefficient depend on elip shape
-    shape_feature(frame, min_elip, elip_angle, elip_ratio, min_rect);
+    shape_feature(frame, motion, elip_angle, elip_ratio);
 
 
     m.lock();
@@ -399,7 +423,6 @@ void run_get_feature(VideoCapture cap, Mat& frame, int& frame_index,
 
 }
 
-//typedef pair<Mat, int> FrameIndex;
 
 struct FrameData{
     Mat frame;
@@ -418,255 +441,6 @@ struct FrameData{
     float elip_ratio;
 };
 
-/*
-struct FrameDataQueue : public queue<FrameData> {
-    mutex lock;
-};
-
-struct FrameQueue : public queue<FrameIndex> {
-    mutex lock;
-};
-
-struct Frame2Queue : public queue<pair<Mat,FrameIndex> > {
-    mutex lock;
-};
-
-struct FrameCompare {
-    bool operator()(const FrameData& x, const FrameData& y)
-    {
-        return x.index > y.index;
-    }
-};
-//typedef priority_queue< FrameData, vector< FrameData >, FrameCompare > PFrameDataQueue;
-
-struct PFrameDataQueue : public priority_queue< FrameData, vector< FrameData >, FrameCompare > {
-    mutex lock;
-};
-
-struct BackgroundSubtraction
-{
-    FrameQueue& frameQueue;
-    Frame2Queue& displayQueue;
-    PFrameDataQueue& pFrameDataQueue;
-    Ptr<BackgroundSubtractor> pMOG2;
-
-    BackgroundSubtraction(FrameQueue& frameQueue_,
-                          Frame2Queue& displayQueue_, PFrameDataQueue& pFrameDataQueue_)
-        : frameQueue(frameQueue_),
-          displayQueue(displayQueue_),
-          pFrameDataQueue(pFrameDataQueue_)
-    {
-        pMOG2 = createBackgroundSubtractorMOG2(300,40,true);
-    }
-
-    void operator()() {
-        while (true) {
-            if (frameQueue.empty()) continue;
-
-
-            FrameData frameData;
-            vector<vector<Point> > contours;
-            vector<Vec4i> hierarchy;
-
-            frameQueue.lock.lock();
-            frameData.frame = frameQueue.front().first;
-            frameData.index = frameQueue.front().second;
-            frameQueue.pop();
-            frameQueue.lock.unlock();
-            MOG2(pMOG2, frameData.frame, frameData.index, frameData.foreground);
-
-            findContours( frameData.foreground, contours, hierarchy, 1, 2);
-            Mat max_contour(frameData.foreground.rows,frameData.foreground.cols,CV_8UC1,Scalar::all(0));
-            frameData.max_contour = max_contour;
-            Draw_max_contour(frameData.foreground,contours,frameData.max_contour,frameData.contour_area,
-                             frameData.min_elip, frameData.min_rect);
-
-            cout<<" index " <<frameData.index<< "frameque" << frameQueue.size() <<"conturo area "<<frameData.contour_area<< endl;
-            imshow("Original",frameData.frame);
-            //imshow("Foreground",frameData.foreground);
-            //waitKey(10);
-
-            pFrameDataQueue.lock.lock();
-            pFrameDataQueue.push(frameData);
-            pFrameDataQueue.lock.unlock();
-
-            //cout << "index=" << FrameData.index << " top " <<pFrameDataQueue.top().index<< " size "  << pFrameDataQueue.size()<< endl;
-
-            //
-            //displayQueue.lock.lock();
-            //displayQueue.push(make_pair(frameData.foreground,make_pair(frameData.frame,frameData.index)));
-            //displayQueue.lock.unlock();
-            //
-        }
-    }
-};
-
-struct Compare {
-    bool operator()(const pair<Mat,FrameIndex>& x, const pair<Mat,FrameIndex>& y)
-    {
-        return x.second.second > y.second.second;
-    }
-};
-
-
-typedef priority_queue< pair<Mat,FrameIndex>, vector< pair<Mat,FrameIndex> >, Compare > PFrame2Queue;
-
-
-struct MHI
-{
-    PFrameDataQueue& pFrameDataQueue;
-    FrameDataQueue* frameDataQueue;
-
-    int nframe;
-
-    MHI(PFrameDataQueue& pFrameDataQueue_, FrameDataQueue* frameDataQueue_)
-        : pFrameDataQueue(pFrameDataQueue_), frameDataQueue(frameDataQueue_), nframe(0)
-    {
-    }
-
-    void operator()() {
-        double t = (double) getTickCount();
-        int needIndex = 1;
-        int n =1;
-        while (true) {
-            //if (!pq.empty()) cout << "top=" << pq.top().second.second << endl;
-
-            if (!pFrameDataQueue.empty() && pFrameDataQueue.top().index == needIndex) {
-
-                pFrameDataQueue.lock.lock();
-                FrameData elem = pFrameDataQueue.top();
-                update_mhi(elem.max_contour,elem.motion,30);
-                imshow("Original",elem.foreground);
-                frameDataQueue[elem.index%n].push(elem);
-                pFrameDataQueue.pop();
-                pFrameDataQueue.lock.unlock();
-                waitKey(1);
-                needIndex++;
-                //cout << "elem= " << elem.index <<" need "<< needIndex <<" size "<< pFrameDataQueue.size() <<endl;
-                //cout << "que1= " << frameDataQueue[0].size() << endl;
-            }
-
-            double framePerSecond = nframe / ( ((double)getTickCount() - t)/getTickFrequency() );
-            //cout << "nframe=" << nframe << " " << framePerSecond << " fps" << endl;
-        }
-    }
-};
-
-struct CaculateCoefficient
-{
-    FrameDataQueue& frameDataQueue;
-    PFrameDataQueue& pFrameDataQueue;
-
-    CaculateCoefficient(PFrameDataQueue& pFrameDataQueue_, FrameDataQueue& frameDataQueue_)
-        : pFrameDataQueue(pFrameDataQueue_), frameDataQueue(frameDataQueue_)
-    {
-    }
-
-    void operator()() {
-        while (true) {
-            //if (!pq.empty()) cout << "top=" << pq.top().second.second << endl;
-
-            if (!frameDataQueue.empty()) {
-            frameDataQueue.lock.lock();
-            FrameData elem = frameDataQueue.front();
-            frameDataQueue.pop();
-            frameDataQueue.lock.unlock();
-            mhi_coefficient(elem.max_contour, elem.motion, 30 , elem.magnitude,
-                             elem.mass_speed, elem.angle, elem.motion_ratio, elem.min_rect);
-            shape_feature(elem.frame, elem.min_elip, elem.elip_angle, elem.elip_ratio, elem.min_rect);
-            pFrameDataQueue.lock.lock();
-            cout << elem.index << " mag " << elem.magnitude << " massS "<< elem.mass_speed
-                << " angle " << elem.angle << " motionR "<< elem.motion_ratio
-                <<" Eangle "<< elem.elip_angle <<" Eratio "<< elem.elip_ratio << endl;
-            pFrameDataQueue.push(elem);
-            cout<< pFrameDataQueue.top().index<< endl;
-            pFrameDataQueue.lock.unlock();
-            }
-        }
-    }
-};
-
-struct Detect
-{
-    PFrameDataQueue& pFrameDataQueue;
-
-    Detect(PFrameDataQueue& pFrameDataQueue_)
-        : pFrameDataQueue(pFrameDataQueue_)
-    {
-    }
-
-    void operator()() {
-        //double t = (double) getTickCount();
-        int needIndex = 1;
-        int n =2;
-        while (true) {
-            //if (!pq.empty()) cout << "top=" << pq.top().second.second << endl;
-            //cout << pFrameDataQueue.top().index << endl;
-            if (!pFrameDataQueue.empty() && pFrameDataQueue.top().index == needIndex) {
-
-                pFrameDataQueue.lock.lock();
-                FrameData elem = pFrameDataQueue.top();
-                //predict(elem.magnitude, elem.mass_speed, elem.angle, elem.motion_ratio,
-                //        elem.elip_angle, elem.elip_ratio, elem.contour_area, elem.index);
-                //imshow("Original",elem.motion);
-                pFrameDataQueue.pop();
-                pFrameDataQueue.lock.unlock();
-                waitKey(1);
-                needIndex++;
-                //cout << "elem= " << elem.index <<" need "<< needIndex <<" size "<< pFrameDataQueue.size() <<endl;
-                //cout << "que1= " << frameDataQueue[0].size() << endl;
-            }
-
-            //double framePerSecond = nframe / ( ((double)getTickCount() - t)/getTickFrequency() );
-            //cout << "nframe=" << nframe << " " << framePerSecond << " fps" << endl;
-        }
-    }
-};
-
-
-struct DisplayImage
-{
-    Frame2Queue& frameQueue;
-    string winname;
-
-    int nframe;
-
-    PFrame2Queue pq;
-
-    DisplayImage(string winname_, Frame2Queue& frameQueue_)
-        : frameQueue(frameQueue_), winname(winname_), nframe(0),
-        pq(Compare())
-    {
-    }
-
-    void operator()() {
-        double t = (double) getTickCount();
-        int needIndex = 1;
-        while (true) {
-            //if (!pq.empty()) cout << "top=" << pq.top().second.second << endl;
-            if (!pq.empty() && pq.top().second.second == needIndex) {
-                auto elem = pq.top();
-                imshow(winname, elem.first);
-                imshow("Original", elem.second.first);
-                pq.pop();
-                waitKey(1);
-                needIndex++;
-            }
-
-            if (frameQueue.empty()) continue;
-            frameQueue.lock.lock();
-            auto elem = frameQueue.front();
-            frameQueue.pop();
-            frameQueue.lock.unlock();
-
-            pq.push(elem);
-            nframe++;
-            double framePerSecond = nframe / ( ((double)getTickCount() - t)/getTickFrequency() );
-            cout << "nframe=" << nframe << " " << framePerSecond << " fps" << endl;
-        }
-    }
-};
-*/
 
 struct DisplayThread {
     typedef pair<Mat,int> FrameIndex;
@@ -694,7 +468,7 @@ struct DisplayThread {
 //                cout << "checkDisplay win = " << winName << " index = " << curIndex[winName]
 //                     << " top = " << q.top().second << endl;
                 if (q.empty() || q.top().second != curIndex[winName]) continue;
-                cout << "display " << winName << " size = " << q.size() << endl;
+//                cout << "display " << winName << " size = " << q.size() << endl;
 
 //                cout << "doDisplay win = " << winName << " index = " << curIndex[winName] << endl;
 
@@ -765,7 +539,7 @@ struct MHIThread : public  FrameDataPQueue, public LockedThread, public HasDispl
             if (this->empty()  || this->top().index != curIndex) continue;
             lock.lock();
 
-            cout << "mhi size = " << size() << endl;
+//            cout << "mhi size = " << size() << endl;
             FrameData elem = this->top();
             curIndex++;
             this->pop();
@@ -774,14 +548,15 @@ struct MHIThread : public  FrameDataPQueue, public LockedThread, public HasDispl
             update_mhi(elem.max_contour,elem.motion,30);
             mhi_coefficient(elem.max_contour, elem.motion, 30 , elem.magnitude,
                              elem.mass_speed, elem.angle, elem.motion_ratio, elem.min_rect);
-            shape_feature(elem.frame, elem.min_elip, elem.elip_angle, elem.elip_ratio, elem.min_rect);
+            shape_feature(elem.frame, elem.motion, elem.elip_angle, elem.elip_ratio);
             int a = predict(elem.magnitude, elem.mass_speed, elem.angle, elem.motion_ratio,
                     elem.elip_angle, elem.elip_ratio, elem.contour_area, elem.index);
             char message[100];
             sprintf(message, "%d", a);
-            if (a == 2) exit(1);
+//            if (a == 2) exit(1);
 
-//            display("Motion", elem.motion, elem.index);
+            //sleep(0.5);
+            display("Motion", elem.motion, elem.index);
 
             Mat frame = elem.frame.clone();
             cv::putText(frame, message, Point(100,100), cv::FONT_HERSHEY_SIMPLEX, 2, Scalar(255,255,255), 2);
@@ -810,7 +585,7 @@ struct ContourThread : public FrameDataQueue, public LockedThread, public HasDis
             this_thread::sleep_for(chrono::milliseconds(1));
             if (this->empty()) continue;
             lock.lock();
-            cout << "contour size = " << size() << endl;
+//            cout << "contour size = " << size() << endl;
             FrameData data = this->front();
             this->pop();
             lock.unlock();
@@ -858,7 +633,9 @@ int main()
 {
     Mat frame;
     int index = 0;
-    VideoCapture cap("Office/video (7).avi");
+    int n = 1;
+    string filename = "Office/video (" + to_string(n) + ").avi" ;
+    VideoCapture cap(filename);
     if ( !cap.isOpened() ){
          cout << "Cannot open the video file" << endl;
          return -1;
@@ -889,7 +666,10 @@ int main()
         if (frame.empty()) {
             cout << "Empty frame" << endl;
             cap.release();
-            cap.open("Office/video (7).avi");
+            mhi_timestamp = 0;
+            n++;
+            filename = "Office/video (" + to_string(n) + ").avi" ;
+            cap.open(filename);
             continue;
         }
 
@@ -910,74 +690,6 @@ int main()
     for (auto& t : allThreads) t.join();
 }
 
-/*
-int tmp1_main()
-{
-    VideoCapture cap("Office/video (7).avi");
-    if ( !cap.isOpened() ){
-         cout << "Cannot open the video file" << endl;
-         return -1;
-    }
-    Mat frame;
-
-    namedWindow("Original");
-    //namedWindow("Foreground");
-
-    int n = 1;
-    FrameQueue frameQueue[n];
-    Frame2Queue displayQueue;
-    FrameDataQueue frameDataQueue[n];
-    PFrameDataQueue pFrameDataQueue, pFrameDataQueueDetect;
-
-    vector<thread> allThreads;
-    //allThreads.push_back(thread(DisplayImage( "Foreground", displayQueue)));
-    for (int i = 0; i < n; i++) {
-        allThreads.push_back(thread( BackgroundSubtraction(ref(frameQueue[i]), ref(displayQueue), ref(pFrameDataQueue)) ));
-    }
-
-    allThreads.push_back(thread(MHI(pFrameDataQueue,frameDataQueue)));
-
-    for (int i = 0; i < n; i++) {
-        allThreads.push_back(thread( CaculateCoefficient(ref(pFrameDataQueueDetect), ref(frameDataQueue[i])) ));
-    }
-
-    allThreads.push_back(thread(Detect(pFrameDataQueueDetect)));
-
-    cout << "All windows on" << endl;
-
-    int index = 0;
-    while (true) {
-
-        read_next_frame(cap, frame, index);
-        //cout<< index<< endl;
-        if (frame.empty()){
-            cout<< "frame is emptyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyasdf";
-           break;
-        }
-
-        //cout << "que1= " << frameQueue[0].size() << endl;
-        frameQueue[index%n].lock.lock();
-        frameQueue[index%n].push(make_pair(frame,index));
-        frameQueue[index%n].lock.unlock();
-        //imshow("afdsfffffffffff",frame);
-        waitKey(30);
-    }
-
-    while(true){
-        if(!frameQueue[0].empty()) continue;
-        cout<< "adsfafafds"<<endl;
-        for (auto& t: allThreads){
-            if(t.joinable()){
-                t.join();
-            }
-        }
-        break;
-    }
-
-
-    return 0;
-}
-*/
 int temp_main(int argc, char* argv[])
 {
 
@@ -1009,30 +721,8 @@ int temp_main(int argc, char* argv[])
                         contours, hierarchy, ref(motion), ref(contour_area),
                         ref(magnitude), ref(mass_speed), ref(angle),
                   ref(motion_ratio), ref(min_elip), ref(elip_angle), ref(elip_ratio));
-        /*
-        thread t2(run_get_feature,cap, ref(frame), ref(frame_index), pMOG2, ref(foreground),
-                        contours, hierarchy, ref(motion), ref(contour_area),
-                        ref(magnitude), ref(mass_speed), ref(angle),
-                  ref(motion_ratio), ref(min_elip), ref(elip_angle), ref(elip_ratio));
 
-        thread t3(run_get_feature,cap, ref(frame), ref(frame_index), pMOG2, ref(foreground),
-                        contours, hierarchy, ref(motion), ref(contour_area),
-                        ref(magnitude), ref(mass_speed), ref(angle),
-                  ref(motion_ratio), ref(min_elip), ref(elip_angle), ref(elip_ratio));
-        thread t4(run_get_feature,cap, ref(frame), ref(frame_index), pMOG2, ref(foreground),
-                        contours, hierarchy, ref(motion), ref(contour_area),
-                        ref(magnitude), ref(mass_speed), ref(angle),
-                  ref(motion_ratio), ref(min_elip), ref(elip_angle), ref(elip_ratio));
-
-
-        if (t2.joinable()) t2.join();
-        if (t3.joinable()) t3.join();
-        if (t4.joinable()) t4.join();
-        */
         if (t1.joinable()) t1.join();
-
-        /* elip angle sao lai tinh tu mhi, tinh motion ratio*/
-
 
 
 
